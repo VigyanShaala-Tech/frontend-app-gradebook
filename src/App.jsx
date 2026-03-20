d5b3687c21ebc71b7eeadf74e245a8e58a929d0b
Sonu-TitanEd <SonuK@TitanEd.com>
2026-03-21 00:08:54 +0530

    add restriction page

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,45 @@ import store from 'data/store';
 import GradebookPage from 'containers/GradebookPage';
 import './App.scss';
 import Head from './head/Head';
+import { useState, useEffect } from 'react';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import RestrictionPage from './components/restriction-page/RestrictionPage';
+
+const RestrictionWrapper = () => {
+  const [hasProfileCompleted, setHasProfileCompleted] = useState(true);
+  const [canAccessPage, setCanAccessPage] = useState(true);
+
+  useEffect(() => {
+    const { LMS_BASE_URL } = getConfig();
+
+    const loadProfileCompletion = async () => {
+      try {
+        const client = getAuthenticatedHttpClient();
+        const { data } = await client.get(`${LMS_BASE_URL}/profile/progress/?role=student`);
+        if (data?.percentage === 100) {
+          setHasProfileCompleted(true);
+        } else {
+          setHasProfileCompleted(false);
+        }
+        setCanAccessPage(data.hidden);
+
+      } catch (err) {
+        console.error('Failed to load profile progress:', err);
+        setHasProfileCompleted(false);
+      }
+    };
+
+    loadProfileCompletion();
+  }, []);
+
+  if (!hasProfileCompleted && !canAccessPage) {
+    return <RestrictionPage />;
+  }
+};
 
 const App = () => (
   <AppProvider store={store}>
+    <RestrictionWrapper />
     <Head />
     <div>
       <Header />
